c31d05e5a51bc945b28e6844f7b997e30088e115
Joseph Brady <escalade2005@hotmail.com>
2015-04-18 12:45:13 -0500

    Initial commit

---
+++ hrSplit-Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace hrSplit_Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numCases = Int32.Parse(Console.ReadLine());
+            var regex = new Regex(@"(\d{1,3})[- ](\d{1,3})[- ](\d{4,10})", RegexOptions.Compiled);
+            while (numCases-- > 0)
+            {
+                var str = Console.ReadLine();
+                var matches = regex.Match(str).Groups;
+                var answer = String.Format("CountryCode{0},LocalAreaCode={1},Number={2}",
+                    matches[0], matches[1], matches[2]);
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
